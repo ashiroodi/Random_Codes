2b55055899c3ea643861e186fe4c4f8c8f53b247
Amir <matthewshiroodi@gmail.com>
2022-09-21 16:46:54 +1000

    Main.py Have been added codes

---
+++ Main.py
@@ -0,0 +1,8 @@
+end_user_name = input("Enter Your Name : ")
+print("Choosen First Name is " , end_user_name)
+
+end_user_lname = input("Enter Your LName : ")
+print("Choosen Last Name is " , end_user_lname)
+
+end_user_number = str(input("Enter a Number : "))
+print("Choosen Number is " , str(end_user_number))
